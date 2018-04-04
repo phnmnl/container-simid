c90732c6d19b6bf557bf3f0c49688d01089e27f5
seliv55 <selivanov@ub.edu>
2018-04-04 05:54:11 +0200

    adjust test

--- runTest1.sh
+++ runTest1.sh
@@ -8,14 +8,14 @@ wget "https://drive.google.com/uc?export=donwload&id=1v1mFJBsUfyXdqiqwfiwDOSEntx
 mkdir -p files/SW620/
 unzip SW620.zip
 
-runsimid.R -i sw620 -o files/ -z SW620/
+runsimid.R -i sw620 -z SW620/ -o out.csv
 rc=$?; 
 if [[ $rc != 0 ]]; then 
 	echo "R process failed with error $rc"
 	exit $rc; 
 fi
 
-if [ ! -f files/all_info ]; then
+if [ ! -f out.csv ]; then
    	echo "File out_exchanged.csv does not exist, failing test."
    	exit 1
 fi
